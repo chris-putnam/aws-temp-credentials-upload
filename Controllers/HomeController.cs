6331a3ee0ed8d601fb435e807d968d30fb989118
Chris Putnam <chris.putnam@swedenstreet.com>
2021-06-02 22:56:45 -0600

    basic chunked uploader

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,41 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using aws_temp_credentials_upload.Models;
+using Amazon.S3;
+using Amazon.IdentityManagement;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model;
 
 namespace aws_temp_credentials_upload.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IAmazonS3 _s3client;
+        private readonly IAmazonIdentityManagementService _iamClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IAmazonSecurityTokenService _tokenClient;
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            IAmazonS3 s3client,
+            IAmazonIdentityManagementService iamClient,
+            IAmazonSecurityTokenService tokenClient)
         {
             _logger = logger;
+            _s3client = s3client;
+            _iamClient = iamClient;
+            _tokenClient = tokenClient;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var response = await this._tokenClient.GetSessionTokenAsync(new GetSessionTokenRequest() {
+                DurationSeconds = 7200
+            });
+
+            Credentials credentials = response.Credentials;
+            return View(credentials);
         }
 
         public IActionResult Privacy()
